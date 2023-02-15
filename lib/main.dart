0d2a6cc253dc3f8ab188baf998015c77d6cd4ccc
ClarissePivasset <cpivasset@artionet.com>
2023-02-15 15:39:33 +0100

    clean architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-import 'src/app.dart';
+import 'src/core/Presentation/app.dart';
 
 void main() => runApp(const App());
--- lib/src/core/Presentation/app.dart
+++ lib/src/core/Presentation/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'ui/home.dart';
+import 'home.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
